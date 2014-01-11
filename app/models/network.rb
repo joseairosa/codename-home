c4120fcdb7b9eef48ad61e3c5fc3d00c8a378b47
José P. Airosa <me@joseairosa.com>
2014-01-11 21:32:49 +0000

    Cleanup

--- app/models/network.rb
+++ app/models/network.rb
@@ -13,27 +13,6 @@ class Network
     field :source
   end
 
-  def initialize(auth)
-    @provider = auth.provider
-    @uid = auth.uid
-    @name = auth.info.name
-    @oauth_token = auth.credentials.token
-    @oauth_expires_at = Time.at(auth.credentials.expires_at)
-    @source = auth
-  end
-
-  def primary_key
-    {uid: @uid}
-  end
-
-  def to_hash
-    {provider: @provider, uid: @uid, name: @name, oauth_token: @oauth_token, oauth_expires_at: @oauth_expires_at}
-  end
-
-  def save!
-    storage_collection.find(primary_key).upsert(primary_key.merge(to_hash))
-  end
-
   private
 
   def storage_collection
