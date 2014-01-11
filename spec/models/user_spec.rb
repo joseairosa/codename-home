8721077d972519d16bd2225915ded7d94b785abf
José P. Airosa <me@joseairosa.com>
2014-01-11 18:53:05 +0000

    Tests working again

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,6 @@ describe User do
 
   describe '.create_or_retrieve' do
     before do
-      #allow(described_class).to receive(:find_or_create_by).and_return(user)
     end
 
     context 'without a user session' do
@@ -14,52 +13,25 @@ describe User do
 
       context 'when creating a new user' do
         before do
-          expect(described_class).to receive(:find_by_account)
-          expect(described_class).to receive(:create!).with(no_args)
+
         end
 
-        it { expect(subject).to be_true }
+        it {
+          expect(described_class).to receive(:find_by_account)
+          expect(subject).to be_a User
+        }
       end
 
       context 'when retrieving a new user' do
         before do
-          expect(described_class).to receive(:find_by_account)
-          expect(described_class).to_not receive(:create!).with(no_args)
+
         end
 
-        it { expect(subject).to be_true }
+        it {
+          expect(described_class).to receive(:find_by_account)
+          expect(subject).to be_a User
+        }
       end
     end
-
-    #context 'with a user session' do
-    #
-    #  subject { described_class.create_or_retrieve(nil, auth) }
-    #
-    #  context 'when creating a new user' do
-    #    before do
-    #      allow(described_class).to receive(:find_by_facebook_uid).and_return(nil)
-    #    end
-    #
-    #    it {
-    #      expect(described_class).to receive(:find_by_facebook_uid).with(anything)
-    #      expect(described_class).to receive(:create).with(no_args)
-    #      expect(user).to receive(:link_facebook)
-    #
-    #      expect(subject).to be_true
-    #    }
-    #  end
-    #
-    #  context 'when retrieving a new user' do
-    #    before do
-    #      allow(described_class).to receive(:find_by_facebook_uid).and_return(user)
-    #
-    #      expect(described_class).to receive(:find_by_facebook_uid).with(anything)
-    #      expect(described_class).to_not receive(:create).with(anything)
-    #      expect(user).to_not receive(:link_facebook).with(anything)
-    #    end
-    #
-    #    it { expect(subject).to eq user }
-    #  end
-    #end
   end
 end
