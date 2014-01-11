4bc52b27a224b7f595b9957df66eb7d96e989e0a
José P. Airosa <me@joseairosa.com>
2014-01-11 21:33:01 +0000

    Added more tests and improved coverage

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ describe User do
 
   describe '.create_or_retrieve' do
     before do
+      expect(described_class).to receive(:find_by_account).and_call_original
     end
 
     context 'without a user session' do
@@ -12,24 +13,40 @@ describe User do
       subject { described_class.create_or_retrieve(nil, auth) }
 
       context 'when creating a new user' do
-        before do
-
-        end
-
-        it {
-          expect(described_class).to receive(:find_by_account)
-          expect(subject).to be_a User
-        }
+        it { expect(subject).to be_a User }
       end
 
-      context 'when retrieving a new user' do
+      context 'when retrieving an existing user' do
         before do
-
+          expect(described_class).to_not receive(:create!)
         end
 
         it {
-          expect(described_class).to receive(:find_by_account)
-          expect(subject).to be_a User
+          user = User.create
+          user.link_account(auth)
+          expect(subject).to eq user
+          expect(user.networks.first.uid).to eq auth.uid
+        }
+      end
+    end
+
+    context 'with a user session' do
+
+      let(:session) { { user_id: '12345' } }
+
+      subject { described_class.create_or_retrieve(session[:user_id], auth) }
+
+      context 'when setting a network' do
+
+        before do
+          expect(described_class).to_not receive(:create!)
+        end
+
+        it {
+          user = User.create
+          user.link_account(auth)
+          expect(subject).to eq user
+          expect(user.networks.first.uid).to eq auth.uid
         }
       end
     end
